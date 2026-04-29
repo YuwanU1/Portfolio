91f4aa2b5f693d05e0fe9ae6601847809df40a1c
YuwanU1 <yuan122005@gmail.com>
2026-04-29 21:10:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     const galleries = {
         gallery1: [
-            "../img&vid/debut1.JPG","../img&vid/debut2.JPG","../img&vid/debut3.JPG",
-            "../img&vid/debut4.JPG","../img&vid/debut12.JPG","../img&vid/debut6.JPG",
-            "../img&vid/debut11.JPG","../img&vid/debut8.JPG","../img&vid/debut9.JPG",
-            "../img&vid/debut10.JPG","../img&vid/debut7.JPG","../img&vid/debut5.JPG"
+            "debut1.JPG","debut2.JPG","debut3.JPG",
+            "debut4.JPG","debut12.JPG","debut6.JPG",
+            "debut11.JPG","debut8.JPG","debut9.JPG",
+            "debut10.JPG","debut7.JPG","debut5.JPG"
         ],
         gallery2: [ 
-            "../img&vid/debuts8.JPG","../img&vid/debuts5.JPG","../img&vid/debuts3.JPG",
-            "../img&vid/debuts4.JPG","../img&vid/debuts12.JPG","../img&vid/debuts6.JPG",
-            "../img&vid/debuts11.JPG","../img&vid/debuts1.JPG","../img&vid/debuts9.JPG",
-            "../img&vid/debuts10.JPG","../img&vid/debuts7.JPG","../img&vid/debuts2.JPG"],
+            "debuts8.JPG","debuts5.JPG","debuts3.JPG",
+            "debuts4.JPG","debuts12.JPG","debuts6.JPG",
+            "debuts11.JPG","debuts1.JPG","debuts9.JPG",
+            "debuts10.JPG","debuts7.JPG","debuts2.JPG"],
         gallery3: [ 
-            "../img&vid/com1.JPG","../img&vid/com2.JPG","../img&vid/com3.JPG",
-            "../img&vid/com4.JPG","../img&vid/com5.JPG","../img&vid/com6.JPG",
-            "../img&vid/com7.JPG","../img&vid/com8.JPG","../img&vid/com9.JPG",
-            "../img&vid/com10.JPG"],
+            "com1.JPG","com2.JPG","com3.JPG",
+            "com4.JPG","com5.JPG","com6.JPG",
+            "com7.JPG","com8.JPG","com9.JPG",
+            "com10.JPG"],
         gallery4: [
-            "../img&vid/meet1.JPG","../img&vid/meet2.JPG","../img&vid/meet8.JPG",
-            "../img&vid/meet6.JPG","../img&vid/meet12.JPG","../img&vid/meet11.JPG",
-            "../img&vid/meet7.JPG","../img&vid/meet3.JPG","../img&vid/meet9.JPG",
-            "../img&vid/meet10.JPG","../img&vid/meet5.JPG","../img&vid/meet6.JPG"],
+            "meet1.JPG","meet2.JPG","meet8.JPG",
+            "meet6.JPG","meet12.JPG","meet11.JPG",
+            "meet7.JPG","meet3.JPG","meet9.JPG",
+            "meet10.JPG","meet5.JPG","meet6.JPG"],
         gallery5: [
-            "../img&vid/meets1.JPG","../img&vid/meets2.JPG","../img&vid/meets8.JPG",
-            "../img&vid/meets6.JPG","../img&vid/meets12.JPG","../img&vid/meets11.JPG",
-            "../img&vid/meets6.JPG","../img&vid/meets3.JPG","../img&vid/meets9.JPG",
-            "../img&vid/meets10.JPG","../img&vid/meets5.JPG","../img&vid/meets7.JPG"],
+            "meets1.JPG","meets2.JPG","meets8.JPG",
+            "meets6.JPG","meets12.JPG","meets11.JPG",
+            "meets6.JPG","meets3.JPG","meets9.JPG",
+            "meets10.JPG","meets5.JPG","meets7.JPG"],
         gallery6: [
-            "../img&vid/booth1.JPG","../img&vid/booth6.JPG","../img&vid/booth3.JPG",
-            "../img&vid/booth7.JPG","../img&vid/booth5.JPG","../img&vid/booth2.JPG",
-            "../img&vid/booth4.JPG"]
+            "booth1.JPG","booth6.JPG","booth3.JPG",
+            "booth7.JPG","booth5.JPG","booth2.JPG",
+            "booth4.JPG"]
 
     };
 
